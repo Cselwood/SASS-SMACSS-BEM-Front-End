ae476dc14e656c803db4588ea5c8d3e8d85a80f2
CSelwood <cselwood1806@hotmail.co.uk>
2019-05-19 17:37:26 +0100

    Colour exploration, flexbox and grid navbar

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
            ?>
 
           <main>
-               Index
+               
           </main>
 
 <?php
--- layout/_header.php
+++ layout/_header.php
@@ -1,12 +1,12 @@
 <header class="header">
-     <nav class="navigation">
-          <div>
-               <a class="" href="/">A dead simple, responsive boilerplate.</a>
+     <nav class="navigation navigation__top">
+          <div class="navigation__menu">
+               <a class="navigation__logo navigation__logo--positioned" href="/">Chris Selwood</a>
           </div>
-          <ul class="navigation navigation__menu">
-               <li><a class="" href="projects.php">projects</a></li>
-               <li><a class="" href="about.php">about</a></li>
-               <li><a class="" href="contact.php">contact</a></li>
+          <ul class="navigation__menu navigation__bottom">
+               <li class="navigation__listitem"><a href="projects.php">projects</a></li>
+               <li class="navigation__listitem"><a href="about.php">about</a></li>
+               <li class="navigation__listitem"><a href="contact.php">contact</a></li>
           </ul>
      </nav>
 </header>
