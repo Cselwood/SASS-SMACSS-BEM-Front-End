3c0483ec510c0781528e9a4d694c02e302e7081c
CSelwood <cselwood1806@hotmail.co.uk>
2019-05-19 17:03:06 +0100

    Testing header from Git cloned repo, will remove next update and correct

--- layout/_header.php
+++ layout/_header.php
@@ -7,6 +7,7 @@
                <li><a class="" href="projects.php">projects</a></li>
                <li><a class="" href="about.php">about</a></li>
                <li><a class="" href="contact.php">contact</a></li>
+               <p>TestP</p>
           </ul>
      </nav>
 </header>
