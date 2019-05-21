2ccc9ec6cba5d4379534c9bbab7effff91691909
CSelwood <cselwood1806@hotmail.co.uk>
2019-05-21 15:45:24 +0100

    previous: removed normalize for online reference, some testing on index

--- index.php
+++ index.php
@@ -6,7 +6,21 @@
                include "layout/_header.php";
           ?>
           <main>
-               
+               <section class="main__widthfix">
+                    <h1>Freelance Front-End Developer & <br>Designer</h1>
+                    <p class="main__paragraph--sized">First-Class Software Engineering graduate, recent explorer of web development and looking to make your websites more memorable! </p>
+               </section>
+
+               <section class="main__postintro main__postintro--coloured">
+                    test<br>
+                    test<br>
+                    test<br>
+                    test<br>
+                    test<br>
+                    test<br>
+                    test<br>
+                    test<br>
+               </section>
           </main>
 
 <?php
--- layout/_header.php
+++ layout/_header.php
@@ -1,12 +1,18 @@
 <header class="header__wrap">
      <div class="navigation__wrap navigation__wrap--outer">
           <div class="header__logo">
-               <a href="/">CS</a>
+               <a class="header__logo--centered header__logo--sized" href="/">Chris Selwood</a>
           </div>
           <ul class="navigation__wrap">
-               <li><a href="about.php">About</a></li>
-               <li><a href="projects.php">Projects</a></li>
-               <li><a href="contact.php">Contact</a></li>
+               <li class="navigation__item">
+                    <a class="navigation__item--sized" href="about.php">About</a>
+               </li>
+               <li class="navigation__item">
+                    <a class="navigation__item--sized" href="projects.php">Projects</a>
+               </li>
+               <li class="navigation__item">
+                    <a class="navigation__item--sized" href="contact.php">Contact</a>
+               </li>
           </ul>
      </div>
 </header>
